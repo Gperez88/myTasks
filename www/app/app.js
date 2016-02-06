f6b8fb8bb29a70155bdc8387a4466756c6572797
Gperez88 <perezgabriel89@gmail.com>
2016-02-06 12:57:01 -0400

    init

---
+++ www/app/app.js
@@ -0,0 +1,36 @@
+angular.module('myTasks', ['ionic'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+      // Don't remove this line unless you know what you are doing. It stops the viewport
+      // from snapping when text inputs are focused. Ionic handles this internally for
+      // a much nicer keyboard experience.
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider){
+  $stateProvider
+
+    .state('home',{
+      url:"/home",
+      templateUrl: "app/home/home.html"
+    })
+
+    .state('app',{
+      url:"/app",
+      templateUrl:"app/layout/menu-layout.html"
+    });
+
+    //if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/app');
+});
